f444ce39a48976f362871dfa77683f5f63d745e9
Jhonnatan <JhonnatanMacias@gmail.com>
2018-12-12 22:12:31 -0500

    minor changes

--- GamesTests/GamesTests.swift
+++ GamesTests/GamesTests.swift
@@ -21,11 +21,5 @@ class GamesTests: XCTestCase {
         super.tearDown()
     }
     
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
     
 }
--- LoginUITests/LoginUITests.swift
+++ LoginUITests/LoginUITests.swift
@@ -25,8 +25,5 @@ class LoginUITests: XCTestCase {
         HooksUITest.login(app: app)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
 }
